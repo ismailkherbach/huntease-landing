90e4d4442ca88260500032c65b481dce746b8ea8
ismailkherbach <gi_kherbach@esi.dz>
2020-06-08 23:15:45 +0100

    Updates

---
+++ precache-manifest.c9ee5173d66109a9f16b2b9016b22a77.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff7848c2e99733230abf6a113a6f3e7",
+    "url": "/huntease-landing/index.html"
+  },
+  {
+    "revision": "2a769a4c40309e265735",
+    "url": "/huntease-landing/static/css/main.fd0e4eca.chunk.css"
+  },
+  {
+    "revision": "b91a7bdd7d06be634255",
+    "url": "/huntease-landing/static/js/2.580dd4ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/huntease-landing/static/js/2.580dd4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a769a4c40309e265735",
+    "url": "/huntease-landing/static/js/main.bc03c2d7.chunk.js"
+  },
+  {
+    "revision": "fa36a0e7dd6d91369a9f",
+    "url": "/huntease-landing/static/js/runtime-main.7f30792d.js"
+  },
+  {
+    "revision": "3a5b2caa399151b6137c86cd1fd10c35",
+    "url": "/huntease-landing/static/media/Box.3a5b2caa.svg"
+  },
+  {
+    "revision": "8243a606c1e24a45547d2073ee8bf2b5",
+    "url": "/huntease-landing/static/media/Chart-dots.8243a606.svg"
+  },
+  {
+    "revision": "c4181fc718091ff88692e87773dfe9c9",
+    "url": "/huntease-landing/static/media/Crown.c4181fc7.svg"
+  },
+  {
+    "revision": "2dc72db0fce15b0ba27296588704791f",
+    "url": "/huntease-landing/static/media/Stroke-1.2dc72db0.svg"
+  },
+  {
+    "revision": "b387f4f6c2d79a773a375ad6ed86d579",
+    "url": "/huntease-landing/static/media/arrow.b387f4f6.svg"
+  },
+  {
+    "revision": "6b01f2d14838b7aefc33eebe4517c8e7",
+    "url": "/huntease-landing/static/media/awesome-check.6b01f2d1.svg"
+  },
+  {
+    "revision": "445eb6cd552e30c73de1b6d8c70acc57",
+    "url": "/huntease-landing/static/media/awesome-info-circle.445eb6cd.svg"
+  },
+  {
+    "revision": "b336ba58cfe944a5ff6683bc7b29d977",
+    "url": "/huntease-landing/static/media/feat_1.b336ba58.svg"
+  },
+  {
+    "revision": "6946ee527a123f3c4d5a5a66ed397ee4",
+    "url": "/huntease-landing/static/media/feat_2.6946ee52.svg"
+  },
+  {
+    "revision": "e64a1e9eda044c334f6cd5934f0f6594",
+    "url": "/huntease-landing/static/media/feat_3.e64a1e9e.svg"
+  },
+  {
+    "revision": "e64a1e9eda044c334f6cd5934f0f6594",
+    "url": "/huntease-landing/static/media/feat_4.e64a1e9e.svg"
+  },
+  {
+    "revision": "9df5eff1340d8acd7a7db0bb95534451",
+    "url": "/huntease-landing/static/media/header_back.9df5eff1.png"
+  },
+  {
+    "revision": "ab5e62afb5798e6747a0d77a40de9741",
+    "url": "/huntease-landing/static/media/ilust_1.ab5e62af.svg"
+  },
+  {
+    "revision": "dc9139c68cd0d3b04a82016682dcec64",
+    "url": "/huntease-landing/static/media/ilust_2.dc9139c6.svg"
+  },
+  {
+    "revision": "f2047058afaca5d07b657ba6df6f06b8",
+    "url": "/huntease-landing/static/media/ilust_3.f2047058.svg"
+  },
+  {
+    "revision": "94375d0fc90248a39921bd151e4575b4",
+    "url": "/huntease-landing/static/media/ilust_5.94375d0f.svg"
+  },
+  {
+    "revision": "f218b65be679f82074af056911f28054",
+    "url": "/huntease-landing/static/media/ilust_6.f218b65b.svg"
+  },
+  {
+    "revision": "a34c26ead721df7b4f52349d77264b83",
+    "url": "/huntease-landing/static/media/logo-dark.a34c26ea.svg"
+  },
+  {
+    "revision": "978d1af5286f6b616c1e2752b71b01d8",
+    "url": "/huntease-landing/static/media/separator.978d1af5.svg"
+  }
+]);
