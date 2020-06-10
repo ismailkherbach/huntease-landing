3c35e3993d8b603d82e3a72caa4fd2cd2d9f1e8f
ismailkherbach <gi_kherbach@esi.dz>
2020-06-10 16:25:02 +0100

    formulaire

--- src/components/small.components/Popup.jsx
+++ src/components/small.components/Popup.jsx
@@ -1,10 +1,27 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 const Popup = (props) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [email, SetEmail] = useState("");
+  const [phone, SetPhone] = useState("");
+  const [name, SetName] = useState("");
+  const [success, SetSuccess] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
-
+  const send_mail = async () => {
+    const response = await axios.post(
+      "https://huntease-mvp.herokuapp.com/request-demo",
+      {
+        mail: email,
+        phone: phone,
+        name: name,
+      }
+    );
+    if ((response.status = 200)) {
+      SetSuccess(true);
+      console.log("Success !");
+    }
+    console.log(email, name, phone);
+  };
   return (
     <div className="popup-container flex fdr aic jcc">
       <div className="popup flex fdc aic jcc">
@@ -24,13 +41,22 @@ const Popup = (props) => {
           <p>We'll contact you as soon as we receive your request.</p>
           <div className="flex fdc aifs">
             <label>First name</label>
-            <input placeholder="Joe Doe" />
+            <input
+              placeholder="Joe Doe"
+              onChange={(e) => SetName(e.target.value)}
+            />
             <label>Work email</label>
-            <input placeholder="johndoe@company.com" />
+            <input
+              placeholder="johndoe@company.com"
+              onChange={(e) => SetEmail(e.target.value)}
+            />
             <label>Phone number</label>
-            <input placeholder="+1   555 - 555 - 5555 " />
+            <input
+              placeholder="+1   555 - 555 - 5555 "
+              onChange={(e) => SetPhone(e.target.value)}
+            />
           </div>
-          <div className="button-demo flex fdr jcc aic">
+          <div className="button-demo flex fdr jcc aic" onClick={send_mail}>
             Get a free demo
             <img alt="arrow" src={require("../../assets/img/arrow.png")} />
           </div>
