96d99641d84e24568f8551f79010f559d790c42d
ismailkherbach <gi_kherbach@esi.dz>
2020-06-10 21:34:32 +0100

    success_messege

--- src/components/small.components/Popup.jsx
+++ src/components/small.components/Popup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 const Popup = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,6 +7,7 @@ const Popup = (props) => {
   const [name, SetName] = useState("");
   const [success, SetSuccess] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
+
   const send_mail = async () => {
     const response = await axios.post(
       "https://huntease-mvp.herokuapp.com/request-demo",
@@ -18,59 +19,71 @@ const Popup = (props) => {
     );
     if ((response.status = 200)) {
       SetSuccess(true);
+      setTimeout(props.tooglePopup, 3000);
       console.log("Success !");
     }
-    console.log(email, name, phone);
   };
   return (
     <div className="popup-container flex fdr aic jcc">
-      <div className="popup flex fdc aic jcc">
-        <div className="top_left" />
-        <div className="cancel flex aic jcc" onClick={props.tooglePopup}>
-          X
-        </div>{" "}
-        <img
-          className="logo"
-          alt="logo"
-          src={require("../../assets/img/logo-white-retina.svg")}
-        />
-        <div className="popup-inner-container flex fdc aic jcc">
-          <h3 className="text_s24 regular_text white">
-            Interested in growing your sales team today? Send us a request 👇
-          </h3>
-          <p className="text_s16 regular_text grey">
-            We'll contact you as soon as we receive your request.
-          </p>
-          <div className="flex fdc aifs">
-            <label className="text_s16 regular_text white">First name</label>
-            <input
-              className="text_s16 regular_text grey"
-              placeholder="Joe Doe"
-              onChange={(e) => SetName(e.target.value)}
-            />
-            <label className="text_s16 regular_text white">Work email</label>
-            <input
-              className="text_s16 regular_text grey"
-              placeholder="johndoe@company.com"
-              onChange={(e) => SetEmail(e.target.value)}
-            />
-            <label className="text_s16 regular_text white">Phone number</label>
-            <input
-              className="text_s16 regular_text grey"
-              placeholder="+1   555 - 555 - 5555 "
-              onChange={(e) => SetPhone(e.target.value)}
-            />
+      {!success ? (
+        <div className="popup flex fdc aic jcc">
+          <div className="top_left" />
+          <div className="cancel flex aic jcc" onClick={props.tooglePopup}>
+            X
+          </div>{" "}
+          <img
+            className="logo"
+            alt="logo"
+            src={require("../../assets/img/logo-white-retina.svg")}
+          />
+          <div className="popup-inner-container flex fdc aic jcc">
+            <h3 className="text_s24 regular_text white">
+              Interested in growing your sales team today? Send us a request 👇
+            </h3>
+            <p className="text_s16 regular_text grey">
+              We'll contact you as soon as we receive your request.
+            </p>
+            <div className="flex fdc aifs">
+              <label className="text_s16 regular_text white">First name</label>
+              <input
+                className="text_s16 regular_text grey"
+                placeholder="Joe Doe"
+                onChange={(e) => SetName(e.target.value)}
+              />
+              <label className="text_s16 regular_text white">Work email</label>
+              <input
+                className="text_s16 regular_text grey"
+                placeholder="johndoe@company.com"
+                onChange={(e) => SetEmail(e.target.value)}
+              />
+              <label className="text_s16 regular_text white">
+                Phone number
+              </label>
+              <input
+                className="text_s16 regular_text grey"
+                placeholder="+1   555 - 555 - 5555 "
+                onChange={(e) => SetPhone(e.target.value)}
+              />
+            </div>
+            <div className="button-demo flex fdr jcc aic" onClick={send_mail}>
+              Get a free demo
+              <img alt="arrow" src={require("../../assets/img/arrow.png")} />
+            </div>
+            <p className="text_s16 regular_text grey">
+              We care about protecting your data. Here’s our{" "}
+              <b className="text_s16 bold_text white">Privacy Policy.</b>
+            </p>
           </div>
-          <div className="button-demo flex fdr jcc aic" onClick={send_mail}>
-            Get a free demo
-            <img alt="arrow" src={require("../../assets/img/arrow.png")} />
-          </div>
-          <p className="text_s16 regular_text grey">
-            We care about protecting your data. Here’s our{" "}
-            <b className="text_s16 bold_text white">Privacy Policy.</b>
-          </p>
         </div>
-      </div>
+      ) : (
+        <div className="success_popup flex fdc aic jcc white text_s14 light_text">
+          <div className="top_left " />
+          <h2>
+            Thank you for contacting us<br></br> We'll contact you as soon as we
+            receive your request
+          </h2>
+        </div>
+      )}
     </div>
   );
 };
