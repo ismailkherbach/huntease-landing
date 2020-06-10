978e07670c91f0a02b9cc6456707b8dc8eee7a44
ismailkherbach <gi_kherbach@esi.dz>
2020-06-10 17:03:26 +0100

    toglle-navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Collapse, NavbarToggler } from "reactstrap";
 import { UncontrolledCollapse } from "reactstrap";
-
 const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const size = useWindowSize();
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <div className="flex fdr aic jcfs">
-      <div className="navbar logo flex jcc">
+      <div className="navbar logo flex jcc" onClick={toggle}>
         {" "}
         <img alt="logo" src={require("../assets/img/logo-white-retina.svg")} />
       </div>
 
-      {
+      {size.width > 900 && (
         <div className="navbar-items flex fdr jcc ">
           <ul className="flex fdr aic">
             <li>Features</li>
@@ -30,9 +30,60 @@ const Navbar = (props) => {
             Request a Demo
           </div>
         </div>
-      }
+      )}
+      {size.width <= 900 && (
+        <div className="toggle-navbar flex fdc aic jcc" onClick={toggle}>
+          {" "}
+          <img alt="toglle" src={require("../assets/img/toggle.svg")} />
+        </div>
+      )}
+      {isOpen ? (
+        <div className="navbar-items flex fdc jcc aifs ">
+          <ul className="flex fdc aifs">
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Faq</li>
+          </ul>
+          <div
+            className="button-demo flex aic jcc"
+            onClick={props.tooglePopup}
+            id="toggler1"
+          >
+            {" "}
+            Request a Demo
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
+function useWindowSize() {
+  const isClient = typeof window === "object";
 
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
+
+  useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
+    function handleResize() {
+      setWindowSize(getSize());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
 export default Navbar;
