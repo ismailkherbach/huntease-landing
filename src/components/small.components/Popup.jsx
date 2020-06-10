bc7e1b79a0184de3efdee6e829cf46ec6c57408a
ismailkherbach <gi_kherbach@esi.dz>
2020-06-10 20:34:57 +0100

    popup-style

--- src/components/small.components/Popup.jsx
+++ src/components/small.components/Popup.jsx
@@ -35,23 +35,28 @@ const Popup = (props) => {
           src={require("../../assets/img/logo-white-retina.svg")}
         />
         <div className="popup-inner-container flex fdc aic jcc">
-          <h3>
+          <h3 className="text_s24 regular_text white">
             Interested in growing your sales team today? Send us a request 👇
           </h3>
-          <p>We'll contact you as soon as we receive your request.</p>
+          <p className="text_s16 regular_text grey">
+            We'll contact you as soon as we receive your request.
+          </p>
           <div className="flex fdc aifs">
-            <label>First name</label>
+            <label className="text_s16 regular_text white">First name</label>
             <input
+              className="text_s16 regular_text grey"
               placeholder="Joe Doe"
               onChange={(e) => SetName(e.target.value)}
             />
-            <label>Work email</label>
+            <label className="text_s16 regular_text white">Work email</label>
             <input
+              className="text_s16 regular_text grey"
               placeholder="johndoe@company.com"
               onChange={(e) => SetEmail(e.target.value)}
             />
-            <label>Phone number</label>
+            <label className="text_s16 regular_text white">Phone number</label>
             <input
+              className="text_s16 regular_text grey"
               placeholder="+1   555 - 555 - 5555 "
               onChange={(e) => SetPhone(e.target.value)}
             />
@@ -60,9 +65,9 @@ const Popup = (props) => {
             Get a free demo
             <img alt="arrow" src={require("../../assets/img/arrow.png")} />
           </div>
-          <p>
+          <p className="text_s16 regular_text grey">
             We care about protecting your data. Here’s our{" "}
-            <b>Privacy Policy.</b>
+            <b className="text_s16 bold_text white">Privacy Policy.</b>
           </p>
         </div>
       </div>
