a99444cf3e6a79b4501d22142532ab28c303e8ef
ismailkherbach <gi_kherbach@esi.dz>
2020-06-13 13:53:53 +0100

    new-fixes

--- src/components/small.components/switch_bloc_mobile.jsx
+++ src/components/small.components/switch_bloc_mobile.jsx
@@ -4,7 +4,7 @@ const SwitchBlocMobile = ({ text, picture }) => {
   return (
     <div className="switch-feature-button-mobile flex fdr jcfs aic">
       <div className="circle-inner red flex aic jcc">{picture}</div>
-      <h5 className="Text blue text_s18 regular_text">{text}</h5>
+      <h5 className="Text blue text_s18 extra_bold_text">{text}</h5>
     </div>
   );
 };
